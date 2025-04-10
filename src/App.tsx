a4b92809888d6346e8f0630c402e642932922f67
carl.chua <carlchua0701@gmail.com>
2025-04-10 14:37:53 +0800

    feat: add default page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Game from './pages/Game';
 import Scanner from './pages/Scanner';
+import SSR from './pages/SSR';
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-center h-screen">
         <nav>
-          <Link to="/">Scanner</Link> | <Link to="/game">Game</Link>
+          <Link to="/vite-project/scanner">Scanner</Link>
+          {' | '}
+          <Link to="/vite-project/game">Game</Link>
         </nav>
         <Routes>
-          <Route path="/" element={<Scanner />} />
-          <Route path="/game" element={<Game />} />
+          <Route path="/vite-project/scanner" element={<Scanner />} />
+          <Route path="/vite-project/game" element={<Game />} />
+          {/* Add a route for the base path */}
+          <Route path="/vite-project/" element={<SSR />} />
+          {/* Default route */}
+          <Route path="*" element={<SSR />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/SSR.tsx
@@ -0,0 +1,9 @@
+function SSR() {
+  return (
+    <div>
+      <h1>Welcome to SSR with Vite!</h1>
+    </div>
+  );
+}
+
+export default SSR;
